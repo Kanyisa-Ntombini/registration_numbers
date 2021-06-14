deb4f226d3becf6648f4a445010e01a8e7924e5c
Kanyisa Ntombini <71196826+Kanyisa-Ntombini@users.noreply.github.com>
2021-06-14 09:00:46 +0200

    prints out all content in localStorage

--- js/registration_number.js
+++ js/registration_number.js
@@ -1,32 +1,42 @@
 function AddingRegNumbers() {
+    let numberplateError = '';
     let theStorageList = [];
     let theRegNum = '';
     
     function setRegNum(num) {
         theRegNum = num;
-        //return theRegNum;
     }
 
     function setRegList (storageList) {
         theStorageList = storageList;
-        //return theStorageList;
     }
 
     function addToList() {
         if (theRegNum.length > 0) {
             theStorageList.push(theRegNum);
+        } else {
+            numberplateError = 'Please enter the registration number';
         }
-        //return theStorageList;
+    }
+
+    function getRegNum() {
+        return theRegNum;
     }
 
     function getUpdatedRegList() {
         return theStorageList;
     }
 
+    function getNumberPlateError() {
+        return numberplateError;
+    }
+
     return {
         setRegNum,
         setRegList,
         addToList,
-        getUpdatedRegList
+        getUpdatedRegList,
+        getNumberPlateError,
+        getRegNum
     }
 }
